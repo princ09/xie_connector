ba8986f50d02dd1d3d1ca2fce37b61dba8a435f4
Rajkumar Gupta <rajkumargupta9615@gmail.com>
2019-11-19 14:28:06 +0530

    backend for Login,Register and profile

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: 'mongodb://admin:admin1997@geekconnector-shard-00-00-8sewn.mongodb.net:27017,geekconnector-shard-00-01-8sewn.mongodb.net:27017,geekconnector-shard-00-02-8sewn.mongodb.net:27017/test?ssl=true&replicaSet=geekconnector-shard-0&authSource=admin&retryWrites=true&w=majority',
+  secretOrKey: 'Bearer'
+};
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+const passport = require('passport');
+const app = express();
+// Body parser Middleware
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+
+//passport middleware
+app.use(passport.initialize());
+
+//passport config
+require('./config/passport')(passport);
+app.get('/', (req, res) => res.send('Hello World'));
+
+// Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+const port = process.env.PORT || 7506;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
